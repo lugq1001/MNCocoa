82444195add0659ce21061377017e12e1935ef3e
陆广庆 <lugq1001@icloud.com>
2018-03-30 16:43:45 +0800

    no message

--- MNCocoa/Sources/Supporting Files/MNCocoa.h
+++ MNCocoa/Sources/Supporting Files/MNCocoa.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018年 陆广庆. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for MNCocoa_iOS.
 FOUNDATION_EXPORT double MNCocoa_iOSVersionNumber;

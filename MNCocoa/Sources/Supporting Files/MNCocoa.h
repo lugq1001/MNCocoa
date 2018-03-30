fefa1eba85f6c12cbabead4c8d4e0c06d2348d46
陆广庆 <lugq1001@icloud.com>
2018-03-30 18:08:26 +0800

    no message

--- MNCocoa/Sources/Supporting Files/MNCocoa.h
+++ MNCocoa/Sources/Supporting Files/MNCocoa.h
@@ -1,6 +1,6 @@
 //
-//  MNCocoa_iOS.h
-//  MNCocoa_iOS
+//  MNCocoa.h
+//  MNCocoa
 //
 //  Created by 陆广庆 on 2018/1/30.
 //  Copyright © 2018年 陆广庆. All rights reserved.
@@ -8,12 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for MNCocoa_iOS.
-FOUNDATION_EXPORT double MNCocoa_iOSVersionNumber;
+//! Project version number for MNCocoa.
+FOUNDATION_EXPORT double MNCocoaVersionNumber;
 
-//! Project version string for MNCocoa_iOS.
-FOUNDATION_EXPORT const unsigned char MNCocoa_iOSVersionString[];
+//! Project version string for MNCocoa.
+FOUNDATION_EXPORT const unsigned char MNCocoaVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <MNCocoa_iOS/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <MNCocoa/PublicHeader.h>
 
 

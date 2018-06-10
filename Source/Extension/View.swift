9ead6af58c0fb2e4c9fb7a6870c621338c4d2562
lugq1001@icloud.com <lugq1001@icloud.com>
2018-06-10 13:45:11 +0800

    no message

--- Source/Extension/View.swift
+++ Source/Extension/View.swift
@@ -10,6 +10,20 @@ import UIKit
 
 extension UIView {
     
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public func removeSubviews() {
+        self.subviews.forEach { (v) in
+            v.removeFromSuperview()
+        }
+    }
+    
     public var safeInsets: UIEdgeInsets {
         if #available(iOS 11.0, *) {
             return self.safeAreaInsets
---
+++ Source/Extension/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MNCocoa
+//
+//  Created by 陆广庆 on 2018/6/9.
+//  Copyright © 2018年 陆广庆. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+
+    public func popViewController() {
+        self.popViewController(animated: true)
+    }
+    
+}

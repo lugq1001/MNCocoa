af5d00391a98e0089804464d936ff7296855c104
Ayukey <yukeyjc@gmail.com>
2018-06-27 06:30:28 +0800

    no message

--- Source/Extension/DateExtensions.swift
+++ Source/Extension/DateExtensions.swift
@@ -41,4 +41,17 @@ public extension Date {
         let s = self.timeIntervalSince1970
         return s
     }
+    
+    static let iso8601Formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+        return formatter
+    }()
+    
+    public var iso8601: String {
+        return Date.iso8601Formatter.string(from: self)
+    }
 }
